b2b80f07f31c5eb0c02f44c260b5711cf3f48a30
gaganguddi <guddigagan4@gmail.com>
2026-05-03 21:06:43 +0530

    DB update

--- src/main/java/com/fashionstore/util/DBConnection.java
+++ src/main/java/com/fashionstore/util/DBConnection.java
@@ -1,3 +1,39 @@
+//package com.fashionstore.util;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//
+//public class DBConnection {
+//
+//    private static final String URL = "jdbc:mysql://localhost:3306/fashion_store?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+//    private static final String USER = "root";
+//    private static final String PASSWORD = "Gagan@2003";
+//
+//    public static Connection getConnection() {
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//
+//            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+//
+//            if (conn != null) {
+//                System.out.println("✅ DB Connected");
+//            }
+//
+//            return conn;
+//
+//        } catch (Exception e) {
+//            try {
+//                java.io.FileWriter fw = new java.io.FileWriter("c:/Users/user/eclipse-workspace/db_error.txt");
+//                java.io.PrintWriter pw = new java.io.PrintWriter(fw);
+//                e.printStackTrace(pw);
+//                pw.close();
+//            } catch(Exception ex) {}
+//            System.out.println("❌ DB Connection Failed");
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//}
 package com.fashionstore.util;
 
 import java.sql.Connection;
@@ -5,9 +41,9 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/fashion_store?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://tramay.proxy.rlwy.net:38862/railway?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "Gagan@2003";
+    private static final String PASSWORD = "waBNmOFhFnYTBVXGVyuZaEqfYiCEbGp";
 
     public static Connection getConnection() {
         try {
@@ -16,18 +52,12 @@ public class DBConnection {
             Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
 
             if (conn != null) {
-                System.out.println("✅ DB Connected");
+                System.out.println("✅ DB Connected Successfully");
             }
 
             return conn;
 
         } catch (Exception e) {
-            try {
-                java.io.FileWriter fw = new java.io.FileWriter("c:/Users/user/eclipse-workspace/db_error.txt");
-                java.io.PrintWriter pw = new java.io.PrintWriter(fw);
-                e.printStackTrace(pw);
-                pw.close();
-            } catch(Exception ex) {}
             System.out.println("❌ DB Connection Failed");
             e.printStackTrace();
         }
